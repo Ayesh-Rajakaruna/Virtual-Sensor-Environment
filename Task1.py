f50e47c5ab5ebf6dff3061aaf6ec68d81fa67e48
Ayesh-Rajakaruna <ayeshrajakaruna@gmail.com>
2022-09-17 17:41:35 +0530

    add the break condition

--- Task1.py
+++ Task1.py
@@ -2,7 +2,7 @@ import time
 import paho.mqtt.client as paho
 import numpy as np
 import threading
-
+import keyboard
 class mqtt_server:
     def __init__(self):
         # Server information
@@ -12,11 +12,18 @@ class mqtt_server:
         self.client_id = 'Group_13'
         self.username = 'app_client'
         self.password = 'app@1234'
+        self.break_condition = 0
         # configure mqtt client
         self.client = paho.Client(self.client_id)
         self.client.username_pw_set(self.username, self.password)
         print("connecting to broker ", self.broker)
         self.client.connect(self.broker, keepalive=60) #connect
+
+    def change_break_condition(self):
+        while(True):
+            if keyboard.is_pressed('q'):
+                self.break_condition = 1
+                return
 
     def thread_function(self, Inizialization_val, lower_val, upper_val, name, seed, sub_topic, dealay): #Fuction for randomly genarate a sensor value and public thos value within 10s dealay
         rng = np.random.default_rng(seed)
@@ -34,11 +41,15 @@ class mqtt_server:
             print("Publishing {} value: {}".format(name, sensor_val))
             self.client.publish(sensor_topic, sensor_val, qos = 0) # Public
             sensor_previous_val = sensor_val #assigning sensor value for sensor previous value
+            if self.break_condition == 1:
+                break
             time.sleep(dealay)
+            
         self.client.disconnect() #disconnect
             
 if __name__ == "__main__":
     server = mqtt_server()
+    break_loop = threading.Thread(target=server.change_break_condition, args=())
     Gas = threading.Thread(target=server.thread_function, args=(35,30,40, 'Gas Sensors', 1, "/Gas01", 10))
     Smoke = threading.Thread(target=server.thread_function, args=(35,30,40,'Smoke Status', 2, "/Smoke01", 10))
     Security = threading.Thread(target=server.thread_function, args=(35,30,40,'Security System Status', 3, "/Security01", 10))
@@ -46,6 +57,7 @@ if __name__ == "__main__":
     Humidity = threading.Thread(target=server.thread_function, args=(35,30,40,'Humidity sensor', 5, "/Humidity01", 10 ))
     Motion = threading.Thread(target=server.thread_function, args=(35,30,40,'Motion Sensors', 6, "/Motion01", 10 ))
     Power = threading.Thread(target=server.thread_function, args=(35,30,40,'Power consumption', 7, "/Power01", 10))
+    break_loop.start()
     Gas.start()
     Smoke.start()
     Security.start()
